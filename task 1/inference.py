4581de129ea5d5792a98d98a3bf1897f7365eb57
Lev Kadatskyi <levkadatscky@gmail.com>
2025-01-30 14:28:54 +0200

    added comments

--- task 1/inference.py
+++ task 1/inference.py
@@ -2,11 +2,14 @@ from transformers import AutoModelForTokenClassification, AutoTokenizer, pipelin
 
 model_path = "task 1/saved_model"
 
+# id to label for better understanding
 id2label = {0: "O", 1: "B-MNTN", 2: "I-MNTN"}
 
+# define model and tokenizer
 model = AutoModelForTokenClassification.from_pretrained(model_path, id2label=id2label)
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 
+# define pipeline for model inference
 ner_pipeline = pipeline("ner", model=model, tokenizer=tokenizer)
 
 sentence = "The Aconcagua is the highest mountain in the world."
